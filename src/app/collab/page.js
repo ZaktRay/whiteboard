5f899c274715b5f864fc19997b9a0c05d07e54b0
ZaktRay <zakt7737@gmail.com>
2025-11-08 19:44:17 +0530

    added collab mode

---
+++ src/app/collab/page.js
@@ -0,0 +1,18 @@
+'use client'
+import { Tldraw } from 'tldraw'
+import { useSyncDemo } from '@tldraw/sync'
+import 'tldraw/tldraw.css'
+import { useSearchParams} from "next/navigation";
+
+export default function Collab() {
+    const searchParams = useSearchParams();
+    const store = useSyncDemo({ roomId: searchParams.get("roomId") });
+
+
+  return (
+    <div className='fixed inset-0'>
+      <Tldraw store={store} />
+    </div>
+  )
+}
+
